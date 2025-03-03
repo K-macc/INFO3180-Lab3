bafdbe9d29ed957eb197fa731f290b2f9045d574
Keshawn McGrath <mkeshawn107@gmail.com>
2025-03-02 23:24:32 -0500

    Form is created and sends an email after submission

--- app/forms.py
+++ app/forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import InputRequired
 
 class ContactForm(FlaskForm):
-    name = StringField('Name', validators=[InputRequired()])
-    email = StringField('Email', validators=[InputRequired()])
-    subject = StringField('Subject', validators=[InputRequired()])
-    message = TextAreaField('Message', validators=[InputRequired()])
+    name = StringField('Name', render_kw={"style": "height: 40px; border-radius: 5px; border: 2px solid #333;"}, validators=[InputRequired()])
+    email = StringField('Email', render_kw={"style": "height: 40px; border-radius: 5px; border: 2px solid #333;"}, validators=[InputRequired()])
+    subject = StringField('Subject', render_kw={"style": "height: 40px; border-radius: 5px; border: 2px solid #333;"}, validators=[InputRequired()])
+    message = TextAreaField('Message', render_kw={"style": "height: 120px; border-radius: 5px; resize: none; border: 2px solid #333;"}, validators=[InputRequired()])
     submit = SubmitField('Submit')
--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,11 @@
-/* Add your Application JavaScript */
+setTimeout(function() {
+    let messages = document.querySelectorAll('.flash-message');
+    messages.forEach(msg => {
+        msg.style.opacity = '0';  // Start fading out
+        
+        // Wait for the CSS transition (500ms) before removing the message
+        setTimeout(() => {
+            msg.remove(); // Remove the message from the DOM
+        }, 500); // Match the transition duration
+    });
+}, 3000); // Start after 5 seconds
